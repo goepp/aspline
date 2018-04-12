3c6483d6e2546273a43a3dfcca26fcaea7501a20
Vivien Goepp <vivien.goepp@gmail.com>
2018-04-12 17:52:03 +0200

    add wridge_solver function

--- R/hello.R
+++ R/hello.R
@@ -12,20 +12,35 @@ NULL
 #' @param w Vector of weights. Has to be of length
 #' @param diff The order of the differences of the parameter. Equals \code{degree + 1} in adaptive spline regression.
 #' @return The solution of the linear system
+
+#' @export
 hessian_solver <- function(par, XX_band, Xy, pen, w, diff) {
   if (ncol(XX_band) != diff + 1) stop("Error: XX_band must have diff + 1 columns")
   bandsolve(XX_band + pen * band_weight(w, diff), Xy) - par
 }
+#' Fit B-Splines with weighted penalization over differences of parameters
+#'
+#' @param XX_band The matrix \code{t(X) %*% X} where \code{X} is the design matrix. This argument is given
+#' in the form of a band matrix, i.e., successive columns represent superdiagonals.
+#' @param Xy The vector of currently estimated points \code{t(X) %*% y}, where \code{y} is the y-coordinate of the data.
+#' @param degree The degree of the B-splines.
+#' @param pen Positive penalty constant.
+#' @param w Vector of weights. The case \eqn{\mathbf w = \mathbf 1} corresponds to fitting P-splines with difference #' order \code{degree + 1} (see \emph{Eilers, P., Marx, B. (1996) Flexible smoothing with B-splines and penalties}.)
+#' @param old_par Initial parameter to serve as starting point of the iterating process.
+#' @param maxiter Maximum number of Newton-Raphson iterations to be computed.
+#' @return The estimated parameter of the spline regression.
+#' @export
 wridge_solver_fast_2 <- function(XX_band, Xy, degree, pen,
                                  w = rep(1, nrow(XX_band) - degree - 1),
                                  old_par = rep(1, nrow(XX_band)),
-                                 maxiter = 1000) {
+                                 maxiter = 1000,
+                                 tol = 1e-8) {
   for (iter in 1:maxiter) {
-    par <- old_par + hessian_solver_fast_2(old_par, XX_band, Xy,
+    par <- old_par + hessian_solver(old_par, XX_band, Xy,
                                            pen, w, diff = degree + 1)
     idx <- old_par != 0
     rel_error <- max(abs(par - old_par)[idx] / abs(old_par)[idx])
-    if (rel_error < 1e-8) break
+    if (rel_error < tol) break
     old_par <- par
   }
   par
