f0e3331f31518c4291b510252ecea9f9a8d7f610
Vivien Goepp <vivien.goepp@gmail.com>
2018-04-26 14:42:30 +0200

    add montreal data

--- R/data.R
+++ R/data.R
@@ -5,9 +5,21 @@
 #'
 #' @format A data frame with 132 rows and 2 variables:
 #' \describe{
-#'   \item{x}{Time afetr impact, in milliseconds}
+#'   \item{x}{Time after impact, in milliseconds}
 #'   \item{y}{Head acceleration, in units of \eqn{g}}
 #'   ...
 #' }
 #' @source Dataset number \eqn{338} of \emph{Hand, D. et al. (1993) A Handbook of Small Datasets}.
 "helmet"
+#' Montreal Temperature Data
+#'
+#' A dataset containing the tempature in Montreal for two years
+#'
+#' @format A data frame with 730 rows and 2 variables:
+#' \describe{
+#'   \item{day}{The day of the year from January 1, 1961, to December 31, 1962}
+#'   \item{temp}{Temperature in Celsius}
+#'   ...
+#' }
+#' @source \url{http://people.stat.sfu.ca/~cschwarz/Consulting/Trinity/Phase2/TrinityWorkshop/Workshop-material-Simon/Intro_to_splines/Exercises/}
+"montreal"
