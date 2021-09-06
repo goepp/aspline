ccf28ec4df855baa7b1e43cbafddb579d8bc2c6c
Vivien <vivien.goepp@gmail.com>
2021-09-06 15:55:59 +0200

    rm block_design_old

--- R/utility.R
+++ R/utility.R
@@ -15,20 +15,6 @@ block_design <- function(X, degree) {
   B <- matrix(X[support_by_row], nrow(X), m, byrow = TRUE)
   list(B = B, alpha = alpha)
 }
-block_design_old <- function(X, degree) {
-  m <- degree + 1
-  X_epsi <- '[<-'(
-    '[<-'(X, 1, 1:m, 1e-100),
-    nrow(X), (ncol(X) - m + 1):ncol(X), 1e-100)
-  support_by_col <- which(X_epsi != 0, arr.ind = TRUE)
-  support_by_row <- support_by_col[order(support_by_col[, 1]), ]
-  X0 <- X * 0
-  X0[support_by_row] <-  1
-  decalage <- apply(X0, 1, function(a) which(a != 0)[1])
-  alpha <- c(which(c(1, diff(decalage)) == 1), nrow(X) + 1)
-  B <- matrix(X[support_by_row], nrow(X), m, byrow = TRUE)
-  list(B = B, alpha = alpha)
-}
 first_non_zero <- function(vect) {
   return(which(vect != 0)[1])
 }
